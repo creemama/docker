0b54f6181a56f6d1d371293357d6052688eb0bee
Chris Topher <c@creemama.com>
2022-11-27 05:36:22 -0500

    chore: add build and push to shellutil-dev/dev.sh

--- shellutil-dev/dev.sh
+++ shellutil-dev/dev.sh
@@ -16,12 +16,22 @@ fi
 . ../shellutil/updateutil.sh
 # set -o xtrace
 
+build() {
+	local version
+	version="$(head -n 1 docker/Dockerfile | sed -E "s#.*:(.*)#\1#")"
+	docker build --tag "creemama/shellutil-dev:$version" docker
+	docker tag "creemama/shellutil-dev:$version" creemama/shellutil-dev:lts-alpine
+	docker images | grep shellutil-dev
+}
+
 main() {
 	# shellcheck disable=SC2039
 	local command_help
-	command_help='docker-format - Format shell scripts and Markdown files.
+	command_help='build - Build the creemama/shellutil-dev Docker image.
+docker-format - Format shell scripts and Markdown files.
 docker-update - Run update using a Docker container.
 git - Run git.
+push - Push creemama/shellutil-dev to Docker Hub.
 update - Check for a newer version of nginx:stable-alpine and update this project if so.'
 	# shellcheck disable=SC2039
 	local commands
@@ -30,21 +40,32 @@ update - Check for a newer version of nginx:stable-alpine and update this projec
 	if [ -z "${1:-}" ]; then
 		main_exit_with_no_command_error "$command_help"
 	elif [ "$1" = "$(arg 0 $commands)" ]; then
+		build
+	elif [ "$1" = "$(arg 1 $commands)" ]; then
 		(
 			# We go up one directory to give the Docker container access to shellutil.
 			cd ..
 			shellutil/format.sh docker-shell-format shellutil-dev
 		)
-	elif [ "$1" = "$(arg 1 $commands)" ]; then
-		run_docker_update
 	elif [ "$1" = "$(arg 2 $commands)" ]; then
+		run_docker_update
+	elif [ "$1" = "$(arg 3 $commands)" ]; then
 		shift
 		../shellutil/git.sh git "$@"
-	elif [ "$1" = "$(arg 3 $commands)" ]; then
+	elif [ "$1" = "$(arg 4 $commands)" ]; then
+		push
+	elif [ "$1" = "$(arg 5 $commands)" ]; then
 		update
 	else
 		main_exit_with_invalid_command_error "$1" "$command_help"
 	fi
+}
+
+push() {
+	local version
+	version="$(head -n 1 docker/Dockerfile | sed -E "s#.*:(.*)#\1#")"
+	docker push "creemama/shellutil-dev:$version"
+	docker push creemama/shellutil-dev:lts-alpine
 }
 
 run_docker_update() {
